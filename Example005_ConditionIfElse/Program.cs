fcec76eef8f8b9cfa2707e9664cac92e00d4ad62
Rumyanceva <nj89.89@mail.ru>
2022-08-02 21:37:38 +0300

    Add Example005_ConditionIfElse project

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оля")
+{
+    Console.WriteLine("Ура! Это же Оля!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
