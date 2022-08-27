a9a474e20025bb7a4a6051781ff7febbc8c49303
Rumyanceva <nj89.89@mail.ru>
2022-08-27 14:00:50 +0300

    Add Example014

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -122,9 +122,9 @@ Console.WriteLine($"{i}! = {Factorial(i)}");
 
 double Fibonacci(int n)
 {
-    if(n == 1 || n == 2) return 1;
+    if (n == 1 || n == 2) return 1;
     else return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
+}  
 
 for (int i = 1; i < 50; i++)
 {
