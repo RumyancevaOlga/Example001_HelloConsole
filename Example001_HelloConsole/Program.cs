9a1ee7906a5aa5cc234a0646c68339ac167805e4
Rumyanceva <nj89.89@mail.ru>
2023-01-04 11:56:19 +0300

    Add QuickSort

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("2 Hello, World!");
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"{a > b}");
+}
+else
+{
+    Console.WriteLine($"{b < a}");
+}
+if (a == b)
+{
+    Console.WriteLine($"{a == b}");
---
+++ QuickSort/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] arr = {0, -5, 2, 3, 5, 9, -1, 7};
+int[] res = QuickSort(arr, 0, arr.Length - 1);
+Console.Write($"Отсортированный массив {string.Join(", ", res)}");
+
+int[] QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if(minIndex >= maxIndex) return inputArray;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return inputArray;
+}
+
+int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for(int i = minIndex; i <= maxIndex; i++)
+    {
+        if(inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex++;
+            Swap(ref inputArray[pivotIndex], ref inputArray[i]);
+        }
+    }
+    pivotIndex++;
+    Swap(ref inputArray[pivotIndex], ref inputArray[maxIndex]);
+    return pivotIndex;
+}
+
+void Swap(ref int leftValue, ref int rightValue)//ref ссылочный вызов
+{
+    int temp = leftValue;
+    leftValue = rightValue;
+    rightValue = temp;
+}
